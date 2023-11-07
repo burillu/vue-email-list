f757142d2d9e251edae0e3685694ece66b4a56bb
burillu <burillu90@hotmail.it>
2023-11-07 16:23:33 +0100

    add list,  show only when all printed

--- js/script.js
+++ js/script.js
@@ -2,15 +2,35 @@
 
 const { createApp } = Vue
 
-  createApp({
-    data() {
-      return {
-        message: 'Hello Vue!'
-      }
-    },
-    mounted(){
-      axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp)=>{
-        console.log(resp);
-      })
+createApp({
+  data() {
+    return {
+      apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+      emailList: []
     }
-  }).mount('#app')
+  },
+  mounted() {
+    for (let index = 0; index < 10; index++) {
+    axios.get(this.apiUrl).then((resp) => {
+      const email = resp.data.response
+      this.emailList.push(email);
+
+    })
+  }
+  },
+  computed:{
+    // emailList(){
+    //   const list=[];
+    //   for (let index = 0; index < 10; index++) {
+    //     //const element = array[index];
+    //      axios.get(this.apiUrl).then((resp) => {
+    //     const email = resp.data.response
+    //     list.push(email);
+  
+    //   })
+    //     return list;
+    //   }
+     
+    // }
+  }
+}).mount('#app')
